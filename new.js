072c8e314b387fb4d637c942185e741e1e59526c
hassan <hassangbenle@gmail.com>
2025-02-28 12:20:40 +0100

    pictureupdate

---
+++ new.js
@@ -0,0 +1,15 @@
+// console.log("SS2SB")
+// // alert("SS2SB")
+
+// function greetStudents() {
+//     alert("Hello Students")
+// }
+
+// greetStudents()
+
+function calculateArea(length, width) {
+    return length * width
+}
+
+const area = calculateArea(5, 10)
+console.log(area)
